3541efae9819bbe52c704764e8eb71d56a646aea
Anvay Mathur <suggestions@megagames.me>
2022-07-26 17:19:32 +0800

    first commit

---
+++ src/lib/header/Header.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<header>
+	<div class="hidden active items"><a href=""></a></div>
+	<nav class="bg-gray-900 shadow-xl flex justify-between items-center py-4 text-white px-6">
+		<!-- Left -->
+		<div class="flex gap-x-5">
+			<div> <!-- Website name/logo -->
+				<h1 class="font-semibold tracking-tight text-3xl">SocialBook</h1>
+			</div>
+			<div class="flex items-center gap-x-3 text-lg items"> <!-- Page links-->
+				<a href="/" class:active="{$page.url.pathname === "/"}">Home</a>
+				<a href="/about" class:active="{$page.url.pathname === "/about"}">About</a>
+			</div>
+		</div>
+
+		<!-- Right  -->
+	</nav>
+</header>
+
+<style>
+
+
+	
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	// import Counter from '$lib/Counter.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1 class="text-5xl">Hello, there.</h1>
+</section>
+
