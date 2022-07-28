99550f5e38afe8a7c6de838b308cfc67a98a6e81
Anvay Mathur <suggestions@megagames.me>
2022-07-28 11:32:35 +0800

    Add db helpers, not implemented properly though
    Co-authored-by: Alan <AlanLikesCoding@users.noreply.github.com>

--- src/lib/data/Data.ts
+++ src/lib/data/Data.ts
@@ -1,64 +1,97 @@
+import { PrismaClient } from "@prisma/client";
+import type { User, Post } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 // temporary structure
-export interface Post {
+// export interface Post {
+//     id: number;
+//     title: string;
+//     creator: string;
+//     content: string;
+//     creatorId: number;
+// }
+
+// export interface User {
+//     id: number;
+//     username: string;
+//     description: string;
+// }
+
+// export interface PostStructure extends Record<string, Post> {}
+
+// export interface UserStructure extends Record<string, User> {}
+
+// let posts: PostStructure = {
+//     "1": {
+//         id: 1,
+//         title: "Sussy Baka",
+//         creator: "Anvay",
+//         content: "I am a frickin sussy baka lol",
+//         creatorId: 1,
+//     },
+//     "2": {
+//         id: 2,
+//         title: "Bussy Saka",
+//         creator: "Azure",
+//         content: "bussy saka sussy baka bussy saka sussy baka",
+//         creatorId: 2,
+//     },
+//     "3": {
+//         id: 3,
+//         title: " Amogus sus lol",
+//         creator: "Anvay",
+//         content: "Dear sir diary, today i played amogus and ate the imposter",
+//         creatorId: 1
+//     },
+//     "4": {
+//         id: 4,
+//         title: "null",
+//         creator: "Azure",
+//         content: "Congratulations! You win absolutely nothing.",
+//         creatorId: 2
+//     }
+// }
+
+// let users: UserStructure = {
+//     "1": {
+//         id: 1,
+//         username: "Anvay",
+//         description: "I am also a human who likes to code.",
+//     },
+//     "2": {
+//         id: 2,
+//         username: "Azure",
+//         description: "I am a human who likes to code.",
+//     }
+// }
+
+export interface PostsResult {
     id: number;
     title: string;
-    creator: string;
-    content: string;
     creatorId: number;
+    creator: {
+        username: string;
+    }
 }
 
-export interface User {
+export interface PostResult extends PostsResult {
+    content: string;
+    creator: {
+        username: string;
+    }
+}
+
+export interface UsersResult {
     id: number;
     username: string;
     description: string;
 }
 
-export interface PostStructure extends Record<string, Post> {}
-
-export interface UserStructure extends Record<string, User> {}
-
-let posts: PostStructure = {
-    "1": {
-        id: 1,
-        title: "Sussy Baka",
-        creator: "Anvay",
-        content: "I am a frickin sussy baka lol",
-        creatorId: 1,
-    },
-    "2": {
-        id: 2,
-        title: "Bussy Saka",
-        creator: "Azure",
-        content: "bussy saka sussy baka bussy saka sussy baka",
-        creatorId: 2,
-    },
-    "3": {
-        id: 3,
-        title: " Amogus sus lol",
-        creator: "Anvay",
-        content: "Dear sir diary, today i played amogus and ate the imposter",
-        creatorId: 1
-    },
-    "4": {
-        id: 4,
-        title: "null",
-        creator: "Azure",
-        content: "Congratulations! You win absolutely nothing.",
-        creatorId: 2
-    }
-}
-
-let users: UserStructure = {
-    "1": {
-        id: 1,
-        username: "Anvay",
-        description: "I am also a human who likes to code.",
-    },
-    "2": {
-        id: 2,
-        username: "Azure",
-        description: "I am a human who likes to code.",
-    }
+export interface UserResult extends UsersResult {
+    posts: {
+        title: string;
+    }[];
 }
 
 export class Data {
@@ -66,26 +99,70 @@ export class Data {
 
     }
 
-    public getPosts(): Post[] {
-        return Object.values(posts);
+    public async getPosts(): Promise<PostsResult[]> {
+        return await prisma.post.findMany({
+            select: {
+                id: true,
+                title: true,
+                creatorId: true,
+                creator: {
+                    select: {
+                        username: true
+                    }
+                }
+            }
+        });
     }
 
-    public getPost(id: number): Post | null {
-        let post = posts[id];
+    public async getPost(id: number): Promise<PostResult | null> {
+        const post = await prisma.post.findFirst({
+            where: {
+                id: id
+            },
+            select: {
+                id: true,
+                title: true,
+                creatorId: true,
+                content: true,
+                creator: {
+                    select: {
+                        username: true
+                    }
+                }
+            }
+        })
         if (post) return post;
-        return null;
+        else return null;
     }
 
-    public getUsers(): User[] {
-        return Object.values(users);
+    public async getUsers(): Promise<UsersResult[]> {
+        return await prisma.user.findMany({
+            select: {
+                id: true,
+                username: true,
+                description: true,
+            }
+        });
     }
 
-    public getUser(id: number): User | null {
-        let user = users[id];
+    public async getUser(id: number): Promise<UserResult | null> {
+        const user = await prisma.user.findFirst({
+            where: {
+                id: id
+            },
+            select: {
+                id: true,
+                username: true,
+                description: true,
+                posts: {
+                    select: {
+                        title: true,
+                    }
+                }
+            }
+        })
         if (user) return user;
-        return null;
+        else return null;
     }
-    public getUserPosts(id: number): Post[] {
-        return Object.values(posts).filter(post => post.creatorId === id);
-    }
+    
 }
